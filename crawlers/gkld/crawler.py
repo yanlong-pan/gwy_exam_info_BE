dc0d580df0ed37c10aa13f93c6912b8a1d1d2d58
pyl <a522723936@gmail.com>
2023-11-28 17:10:02 +0800

    add support for date filtering

--- crawlers/gkld/crawler.py
+++ crawlers/gkld/crawler.py
@@ -1,3 +1,6 @@
+import datetime
+import dateparser
+from functools import partial
 import os
 import re
 import time
@@ -7,18 +10,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException
+from dateutil.relativedelta import relativedelta 
+from utilities import constant, fileIO, flow, timeutil
 
-from utilities import constant, fileIO, flow
-
-def process_province_page(driver: webdriver.Chrome, province_name, exam_type, page_num):
+def process_province_page(driver: webdriver.Chrome, province_name, exam_type, page_num, start_date: datetime.date, end_date: datetime.date):
     #  Jump to specific page number
     url = driver.current_url.split('?')[0] + f'?page={page_num}'
     driver.execute_script(f"window.open('{url}');")
     province_page_with_pagination = flow.switch_to_lastest_window(driver)
+    
+    def is_date_valid(e: WebElement, start_date: datetime.date, end_date: datetime.date):
+        date_str = e.find_element(By.XPATH, './/time').text
+        if '前' in date_str:
+            date_str = dateparser.parse(date_str).strftime("%Y-%m-%d")
+        return not timeutil.is_date_within_range(date_str, start_date, end_date)
 
     @flow.iterate_over_web_elements(
         driver = driver,
-        selector_value = '.notice-list li a'
+        selector_value = '.notice-list li',
+        stop = partial(is_date_valid, start_date=start_date, end_date=end_date)
     )
     @flow.operate_in_new_window(
         driver = driver,
@@ -61,6 +71,13 @@ def scrape_website():
             exam_types: List[str] = driver.find_element(By.XPATH, '//dt[contains(text(),"考试类型")]/following-sibling::dd/ul').text.split()
             num_of_exam_types = len(exam_types)
             for i in range(num_of_exam_types):
+                download_dir = os.path.join(os.getenv('DOWNLOAD_ARTICLES_DIR'), f'./{province_name}/{exam_types[i]}')
+                if not os.path.exists(download_dir):
+                    os.makedirs(download_dir)
+                subdirectories = fileIO.get_subdirectories(depth=2, path=download_dir)
+                end_date: datetime.date = timeutil.get_current_date_in_timezone()
+                start_date: datetime.date= timeutil.extract_max_date(subdirectories) or end_date - relativedelta(months=1)
+
                 driver.find_element(By.XPATH, f'//i[contains(@class, "icon-check")]/following-sibling::a[contains(text(), "{exam_types[i]}")]').click()
                 time.sleep(3)
                 
@@ -77,7 +94,7 @@ def scrape_website():
                     totalPages = min(totalPages, 2)
                 # 处理每个分页
                 for j in range(1, totalPages + 1):
-                    process_province_page(driver, province_name, exam_types[i], j)
+                    process_province_page(driver, province_name, exam_types[i], j, start_date, end_date)
                     driver.switch_to.window(province_page)
                 
             # 关闭新窗口并切换回原始窗口
