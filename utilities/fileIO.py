2a2ed5fb91eab9d758ad7baf97048ce2ff5a071c
pyl <a522723936@gmail.com>
2023-11-28 17:08:48 +0800

    get subdirectories of a certain path with depth

--- utilities/fileIO.py
+++ utilities/fileIO.py
@@ -19,3 +19,27 @@ def write_content_to_file(directory: str, file_name: str, content):
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(content)
+
+def get_subdirectories(depth, path='.'):
+    """
+    Get all subdirectories up to a specified depth.
+
+    Args:
+    depth (int): The depth to search for subdirectories.
+    path (str): The starting directory path. Defaults to the current directory.
+
+    Returns:
+    list: A list of subdirectories.
+    """
+    subdirs = []
+    def recurse(current_path, current_depth):
+        if current_depth > depth:
+            return
+        for item in os.listdir(current_path):
+            full_path = os.path.join(current_path, item)
+            if os.path.isdir(full_path):
+                subdirs.append(full_path)
+                recurse(full_path, current_depth + 1)
+
+    recurse(path, 1)
+    return subdirs
