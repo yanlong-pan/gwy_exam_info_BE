6f95ede8d4ec5fbd7f96c2c4c97c46243307789d
pyl <a522723936@gmail.com>
2023-12-07 18:58:47 +0800

    add ability to search max date from meili

--- search_engine/meilisearch/articles.py
+++ search_engine/meilisearch/articles.py
@@ -1,15 +1,15 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 from search_engine.meilisearch.manager import Manager
 from utilities import Singleton
-
 class Article(BaseModel):
     id: str
     title: str
     province: str
     exam_type: str
     info_type: str
-    collect_date: str
+    collect_date: float
     html_content: str
 
 @Singleton
@@ -18,5 +18,20 @@ class ArticleManager(Manager):
     def __init__(self):
         super().__init__('articles')
         self.index.update_settings({
-            'filterableAttributes': ['province', 'exam_type', 'info_type', 'collect_date'],
+            'filterableAttributes': ['province', 'exam_type', 'info_type', 'collect_date', 'title'],
+            'sortableAttributes': ['collect_date']
         })
+    
+    def get_max_collect_date(self, filters: dict={}):
+        r: dict = self.index.search(
+            query = '*',
+            opt_params = {
+                'filter': [f'{key}={value}' for key, value in filters.items()],
+                'sort': ['collect_date:desc'],
+                'attributesToSearchOn': ['collect_date'],
+                'limit': 1
+            }
+        )
+        return datetime.utcfromtimestamp(r['hits'][0]['collect_date']) if r['hits'] else None
+
+article_manager = ArticleManager()
