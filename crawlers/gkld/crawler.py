fd4395e5e834a9094923ca2bb2c1a7361fc7aa08
pyl <a522723936@gmail.com>
2023-12-08 13:18:49 +0800

    get rid the fileIO, store content into meilisearch directly

--- crawlers/gkld/crawler.py
+++ crawlers/gkld/crawler.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import uuid
 from bs4 import BeautifulSoup
 import dateparser
 from functools import partial
@@ -14,8 +15,8 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from dateutil.relativedelta import relativedelta
-from search_engine.meilisearch.articles import article_manager 
-from utilities import constant, fileIO, flow
+from search_engine.meilisearch.articles import Article, article_manager 
+from utilities import constant, flow, timeutil
 
 def _create_chrome_web_driver(headless: bool = False):
     chrome_options = Options()
@@ -27,7 +28,7 @@ def _create_chrome_web_driver(headless: bool = False):
     driver.implicitly_wait(10)
     return driver
 
-def process_province_page(driver: webdriver.Chrome, province_name, exam_type, info_type, page_num, start_dt: datetime, end_dt: datetime):
+def process_province_page(driver: webdriver.Chrome, province_name, exam_type, info_type, page_num, start_dt_utc: datetime, end_dt_utc: datetime):
     #  Jump to specific page number
     url = driver.current_url.split('?')[0] + f'?page={page_num}'
     driver.execute_script(f"window.open('{url}');")
@@ -43,16 +44,17 @@ def process_province_page(driver: webdriver.Chrome, province_name, exam_type, in
         # 获取<h1>元素的纯文本内容并输出
         return soup.get_text().strip()
     
-    def is_date_invalid(e: WebElement, start: datetime, end: datetime):
+    def is_date_invalid(e: WebElement, start_utc: datetime, end_utc: datetime):
         date_str = e.find_element(By.XPATH, './/time').text
         date_time: datetime = dateparser.parse(date_str) if '前' in date_str else datetime.strptime(date_str, constant.HYPHEN_JOINED_DATE_FORMAT)
         # 统一时区再比较。若datetime对象未设置Timezone，调用astimezone方法默认会先转换本地时区
-        return not start.astimezone(pytz.utc) <= date_time.astimezone(pytz.utc) <= end.astimezone(pytz.utc)
+        is_within_range = start_utc <= date_time.astimezone(pytz.utc) <= end_utc
+        return not is_within_range
        
     @flow.iterate_over_web_elements(
         driver = driver,
         selector_value = '.notice-list li',
-        stop = partial(is_date_invalid, start=start_dt, end=end_dt)
+        stop = partial(is_date_invalid, start_utc=start_dt_utc, end_utc=end_dt_utc)
     )
     @flow.operate_in_new_window(
         driver = driver,
@@ -61,31 +63,27 @@ def process_province_page(driver: webdriver.Chrome, province_name, exam_type, in
     def save_notices():
         date = driver.find_element(By.CLASS_NAME, 'date').get_attribute('innerHTML')
         match = re.search(constant.HYPHEN_JOINED_DATE_REGEX, date)
-        date = match.group().replace('-', '_') if match else 'unknown_date'
+        date = match.group()
 
         article: WebElement = driver.find_element(By.CLASS_NAME, 'article-detail')
-        download_dir = os.getenv('DOWNLOAD_ARTICLES_DIR', './articles') + f'/{province_name}/{exam_type}/{info_type}/{date}'
-        file_name = (get_article_title(driver) + '.html').replace('/', '|')
-        try:
-            attachments = map(
-                lambda e: (e.get_attribute('href'), e.text), 
-                article.find_elements(
-                    By.XPATH, 
-                    './/a[contains(@href, "file") or contains(@href, "attach")][string-length(normalize-space(text())) > 0][@target="_blank"]'
-                )
-            )
-            mapping = {}
-            for (download_url, attachment_name) in attachments:
-                fileIO.download_file_from_url(download_url, download_dir, attachment_name)
-                if file_name not in mapping:
-                    mapping[file_name] = []
-                mapping[file_name].append(attachment_name)
-            if mapping:
-                fileIO.add_mapping_between_attachments_and_article(download_dir, mapping)
-        finally:
+        article_title = get_article_title(driver).replace('/', '|')
+        if article_manager.index.get_documents({'filter': [f'title="{article_title}"']}).total == 0:
+            # TODO: parse content and remove sensitive data, also replace attachments' link
             content = article.get_attribute('innerHTML')
-            fileIO.write_content_to_file(download_dir, file_name, content)
-
+            article_manager.index.add_documents(documents=[{**Article(
+                id=str(uuid.uuid4()),
+                title=article_title,
+                province=province_name,
+                exam_type=exam_type,
+                info_type=info_type,
+                # set the parsed time to local timezone and then convert it to UTC timestamp
+                collect_date=datetime.strptime(date, constant.HYPHEN_JOINED_DATE_FORMAT)
+                    .replace(tzinfo=timeutil.get_tz())
+                    .astimezone(pytz.utc)
+                    .timestamp(),
+                html_content=content
+            ).model_dump()}])
+            
     save_notices()
     # 关闭省份页面
     if driver.current_window_handle == province_page_with_pagination:
@@ -127,13 +125,13 @@ def scrape_website():
                     _click_checkbox(info_types[a_i-1], checked=True)
 
                 for i in range(num_of_exam_types):
-                    end_dt: datetime = datetime.utcnow()
+                    end_dt_utc: datetime = datetime.now().astimezone(pytz.utc)
                     filters={
                         'province': province_name,
                         'exam_type': exam_types[i],
                         'info_type': info_types[a_i],
                     }
-                    start_dt: datetime = article_manager.get_max_collect_date(filters) or end_dt - relativedelta(months=1)
+                    start_dt_utc: datetime = article_manager.get_max_collect_date(filters) or end_dt_utc - relativedelta(months=1)
                     _click_checkbox(exam_types[i])
                     
                     if i > 0:
@@ -148,7 +146,7 @@ def scrape_website():
                         totalPages = min(totalPages, 2)
                     # 处理每个分页
                     for j in range(1, totalPages + 1):
-                        process_province_page(driver, province_name, exam_types[i], info_types[a_i], j, start_dt, end_dt)
+                        process_province_page(driver, province_name, exam_types[i], info_types[a_i], j, start_dt_utc, end_dt_utc)
                         driver.switch_to.window(province_page)
                     
             # 关闭新窗口并切换回原始窗口
