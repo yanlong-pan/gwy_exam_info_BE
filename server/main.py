5ed6bbf759423a7222f68327512d70812b6e2ac9
pyl <a522723936@gmail.com>
2023-12-11 16:53:16 +0800

    det env vars in constant module

--- server/main.py
+++ server/main.py
@@ -1,4 +1,3 @@
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from search_engine.meilisearch.articles import article_manager
 from server.model.body import SearchArticles
@@ -6,7 +5,6 @@ from utilities import timeutil
 from fastapi import FastAPI, Depends
 from server.routes.auth import get_current_client, router as auth_router
 
-load_dotenv()
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -3,19 +3,10 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import ExpiredSignatureError, JWTError, jwt
 from datetime import datetime, timedelta
-import os
+from utilities import constant
 
 # 创建一个 APIRouter 实例
 router = APIRouter()
-
-# 定义环境变量
-CLIENT_ID = os.getenv("CLIENT_ID")  # 使用环境变量或默认值
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-
-# JWT 配置
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -32,16 +23,16 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, constant.SECRET_KEY, algorithm=constant.ALGORITHM)
     return encoded_jwt
 
 @router.post("/token")
 async def token(form_data: OAuth2PasswordRequestForm = Depends()):
     client_id = form_data.username
     client_secret = form_data.password
-    if client_id != CLIENT_ID or client_secret != CLIENT_SECRET:
+    if client_id != constant.CLIENT_ID or client_secret != constant.CLIENT_SECRET:
         raise credentials_exception
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=constant.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": client_id}, expires_delta=access_token_expires
     )
@@ -49,9 +40,9 @@ async def token(form_data: OAuth2PasswordRequestForm = Depends()):
 
 async def get_current_client(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, constant.SECRET_KEY, algorithms=[constant.ALGORITHM])
         client_id: str = payload.get("sub")
-        if client_id is None or client_id != CLIENT_ID:
+        if client_id is None or client_id != constant.CLIENT_ID:
             raise credentials_exception
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token expired")
