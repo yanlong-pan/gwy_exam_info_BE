fba074a1e2878e14b2a59279c18cc264cd39a0d6
pyl <a522723936@gmail.com>
2024-01-18 12:03:59 +0800

    enhance datetime extraction

--- utilities/timeutil.py
+++ utilities/timeutil.py
@@ -24,8 +24,8 @@ def format_date(date_str: str) -> str:
     return formatted_date
 
 def extract_end_dt_with_regex(text: str) -> (bool, str):
-    datetime_pattern = r"(\d{4}.\d{1,2}.?(\d{0,2}.?)?(.?\d{2}:\d{2})?)"
-    datetime_matches = re.findall(datetime_pattern, text.replace('至', '-'))
+    datetime_pattern = r"(\d{4}.\d{1,2}.?(\d{1,2}.?)?(.?\d{1,2}:\d{1,2})?)"
+    datetime_matches = re.findall(datetime_pattern, text)
     try:
         if datetime_matches:
             end_time = format_date(datetime_matches[-1][0])
@@ -44,19 +44,29 @@ def extract_end_dt_with_ai(text: str, collect_date_str: str) -> (bool, str):
     response = requests.get(os.getenv('UNI_APP_AI_CLOUD_URL'), params=params)
     if response.status_code == 200:
         dt = extract_end_dt_with_regex(response.text)
+        print(f"使用AI服务，原输入为{text}，AI解析后为{dt}") # TODO：改为使用logging
         return dt
     else:
         return (False, text)
 
-def extract_end_datetime(text: str, collect_date_str: str):
-    # len('报名时间：2023年1月2日 14:30') = 20, 正则匹配仅处理最简单的情况
-    if len(text) <= 21:
+def extract_end_datetime(text: str, collect_date_str: str, pattern: str = None):
+    # 正则匹配仅处理标准格式的情况
+    # len('报名时间：2023年1月2日 14:30') = 20
+    # 报名时间：2023年1月2日 14:30
+    # 报名时间：2023-10-29 08:00至2023-11-07 18:00
+    # 报名时间：2023-10-29至2023-11-07
+    # 报名时间：2023-10-29 至 2023-11-07
+    # pattern = r"报名时间[：:](\d{4}-\d{2}-\d{2}(?:\s+\d{2}:\d{2})?)(?:\s*至\s*)(\d{4}-\d{2}-\d{2}(?:\s+\d{2}:\d{2})?)"
+    if not pattern:
+        pattern = r"报名时间[：:](\d{4}-\d{1,2}-\d{1,2}(?:\s+\d{1,2}:\d{1,2})?)(?:\s*至\s*)(\d{4}-\d{1,2}-\d{1,2}(?:\s+\d{1,2}:\d{1,2})?)"
+    text = text.replace('官方报名入口', '').strip()
+    if len(text) <= 21 or re.findall(pattern, text):
         isSuccess, result = extract_end_dt_with_regex(text)
         if isSuccess:
             return result
         else:
-            _, r = extract_end_dt_with_ai(text, collect_date_str)
-            return r
+            ok, r = extract_end_dt_with_ai(text, collect_date_str)
+            return r if ok else None
     else:
         _, r = extract_end_dt_with_ai(text, collect_date_str)
         return r
