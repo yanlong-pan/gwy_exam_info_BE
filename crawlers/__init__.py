b286691a4073282216d398e849e4246cde456afa
pyl <a522723936@gmail.com>
2024-06-15 23:15:16 +0800

    config browser path
    driver path configured in PATH

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from utilities import constant
 
 class Crawler(ABC):
 
@@ -10,6 +13,7 @@ class Crawler(ABC):
             chrome_options.add_argument('--headless')  # 启用headless模式
             chrome_options.add_argument("--window-size=1920,1080")  # 设置窗口大小
         # 创建Chrome WebDriver
+        chrome_options.binary_location = constant.CHROME_BROWSER_PATH_FOR_SELENIUM
         driver = webdriver.Chrome(options=chrome_options)
         driver.implicitly_wait(10)
         return driver
